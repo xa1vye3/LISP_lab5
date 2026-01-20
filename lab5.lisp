d78a12fdcaf5e85dc48463dc012d1b1278afb790
xa1vye3 <xa1vylmao@gmail.com>
2026-01-20 04:59:01 +0100

    Add files via upload

---
+++ lab5.lisp
@@ -0,0 +1,210 @@
+(defstruct specialty
+  id
+  name)
+
+(defstruct article
+  id
+  title
+  author
+  year
+  specialty-id)
+
+(defun split-csv-line (line)
+  (loop with start = 0
+        for pos = (position #\, line :start start)
+        collect (string-trim " " (subseq line start pos))
+        while pos
+        do (setf start (1+ pos))))
+
+
+(defun read-csv (path)
+  (with-open-file (in path)
+    (loop for line = (read-line in nil nil)
+          while line
+          collect (split-csv-line line))))
+
+(defun load-specialties (path)
+  (let ((rows (rest (read-csv path))))
+    (mapcar #'make-specialty-from-row rows)))
+
+
+(defun make-specialty-from-row (row)
+  (make-specialty
+   :id (parse-integer (first row))
+   :name (second row)))
+
+(defun make-article-from-row (row)
+  (make-article
+   :id (parse-integer (first row))
+   :title (second row)
+   :author (third row)
+   :year (parse-integer (fourth row))
+   :specialty-id (parse-integer (fifth row))))
+
+(defun select (path row->record)
+  (lambda (&key id title author year specialty-id)
+    (let* ((rows (rest (read-csv path)))
+           (records (mapcar row->record rows)))
+      (remove-if-not
+       (lambda (rec)
+         (and
+          (if id (= id (article-id rec)) t)
+            (if title (string= title (article-title rec)) t)
+            (if author (string= author (article-author rec)) t)
+            (if year (= year (article-year rec)) t)
+            (if specialty-id (= specialty-id (article-specialty-id rec))
+
+              t)))
+       records))))
+
+(defparameter *select-articles*
+  (select "articles.csv" #'make-article-from-row))
+
+
+
+
+(defun find-specialty-name (specialty-id specialties)
+  (let ((spec (find specialty-id specialties
+                    :key #'specialty-id
+                    :test #'=)))
+    (if spec
+        (specialty-name spec)
+        "Unknown specialty")))
+
+
+
+(defun write-records-to-file (records path printer)
+  (with-open-file (out path
+                       :direction :output
+                       :if-exists :supersede)
+    (dolist (r records)
+      (funcall printer r out))))
+
+
+(defun article->csv (a stream)
+  (format stream "~a,~a,~a,~a,~a~%"
+          (article-id a)
+          (article-title a)
+          (article-author a)
+          (article-year a)
+          (article-specialty-id a)))
+
+
+(defun article->hash (a)
+  (let ((h (make-hash-table :test 'equal)))
+    (setf (gethash 'id h) (article-id a)
+          (gethash 'title h) (article-title a)
+          (gethash 'author h) (article-author a)
+          (gethash 'year h) (article-year a)
+          (gethash 'specialty-id h) (article-specialty-id a))
+    h))
+
+
+(defun specialty->hash (s)
+  (let ((h (make-hash-table :test 'equal)))
+    (setf (gethash 'id h) (specialty-id s)
+          (gethash 'name h) (specialty-name s))
+    h))
+
+
+
+
+(defun print-article-with-specialty (article specialties)
+  (format t
+          "~%--------------------------------~%~
+           ID: ~a~%~
+           Name: ~a~%~
+           Author: ~a~%~
+           Year: ~a~%~
+           Specialty: ~a~%"
+          (article-id article)
+          (article-title article)
+          (article-author article)
+          (article-year article)
+          (find-specialty-name
+           (article-specialty-id article)
+           specialties)))
+
+
+
+(defun print-articles-with-specialties (articles specialties)
+  (dolist (a articles)
+    (print-article-with-specialty a specialties)))
+
+
+
+(defparameter *articles-1*
+  (funcall *select-articles* :specialty-id 1))
+
+(defparameter *articles-2*
+  (funcall *select-articles* :specialty-id 2))
+
+(defparameter *articles*
+  (append *articles-1* *articles-2*))
+
+(defparameter *specialties*
+  (load-specialties "specialties.csv"))
+
+(print-articles-with-specialties *articles* *specialties*)
+
+(write-records-to-file
+ *articles*
+ "result.csv"
+ #'article->csv)
+
+(defun check-read-test (test-name file-path row->record expected)
+  (let* ((rows (rest (read-csv file-path)))
+         (records (mapcar row->record rows))
+         (result (equalp records expected))) 
+    (format t "Test: ~A - ~:[FAILED~;PASSED~]~%" test-name result)))
+
+
+(defun check-select-test (test-name select-lambda filter-alist expected)
+  (let ((result (funcall select-lambda
+                         :id (getf filter-alist :id)
+                         :title (getf filter-alist :title)
+                         :author (getf filter-alist :author)
+                         :year (getf filter-alist :year)
+                         :specialty-id (getf filter-alist :specialty-id))))
+    (format t "Test: ~A - ~:[FAILED~;PASSED~]~%" test-name
+            (equalp result expected))))
+
+
+
+(defun run-tests ()
+
+  (check-read-test "Read specialties"
+                   "specialties.csv"
+                   #'make-specialty-from-row
+                   (list (make-specialty :id 1 :name "Computer Science")
+                         (make-specialty :id 2 :name "Applied Mathematics")
+                         (make-specialty :id 3 :name "Physics")))
+  
+  (check-read-test "Read articles"
+                   "articles.csv"
+                   #'make-article-from-row
+                   (list (make-article :id 1 :title "Lisp in Artificial Intelligence" :author "John McCarthy" :year 1960 :specialty-id 1)
+                         (make-article :id 2 :title "Structure and Interpretation of Computer Programs" :author "Harold Abelson" :year 1985 :specialty-id 1)
+                         (make-article :id 3 :title "Lambda Calculus and Functional Programming" :author "Alonzo Church" :year 1936 :specialty-id 2)
+                         (make-article :id 4 :title "Computational Methods in Mathematics" :author "John von Neumann" :year 1947 :specialty-id 2)
+                         (make-article :id 5 :title "Quantum Theory Foundations" :author "Max Planck" :year 1900 :specialty-id 3)
+                         (make-article :id 6 :title "Relativity and Physics" :author "Albert Einstein" :year 1915 :specialty-id 3)))
+  
+
+  (check-select-test "Select article by ID"
+                     (select "articles.csv" #'make-article-from-row)
+                     '(:id 2)
+                     (list (make-article :id 2 :title "Structure and Interpretation of Computer Programs" :author "Harold Abelson" :year 1985 :specialty-id 1)))
+
+  (check-select-test "Select articles by specialty-id"
+                     (select "articles.csv" #'make-article-from-row)
+                     '(:specialty-id 2)
+                     (list (make-article :id 3 :title "Lambda Calculus and Functional Programming" :author "Alonzo Church" :year 1936 :specialty-id 2)
+                           (make-article :id 4 :title "Computational Methods in Mathematics" :author "John von Neumann" :year 1947 :specialty-id 2)))
+  
+  (format t "Full table of articles:~%")
+  (print-articles-with-specialties
+   (funcall (select "articles.csv" #'make-article-from-row))
+   (load-specialties "specialties.csv")))
+
+(run-tests)
